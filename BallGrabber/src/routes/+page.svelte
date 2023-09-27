f49aab1fa0a8656c1d7de81a101a0f0443450ec3
Gabriel Tower <gabriel.tower@baxter-academy.org>
2023-09-27 12:43:49 -0400

    Background and tile formated

--- BallGrabber/src/routes/+page.svelte
+++ BallGrabber/src/routes/+page.svelte
@@ -9,5 +9,14 @@
 
 
 <div class="w-full h-full flex flex-col justify-center items-center">
-	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Ball Grabber</Heading>
+	<div class="h-96"/>
+	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-9xl">Ball Grabber</Heading>
+
 </div>
+
+<style>
+	:global(body){
+        background-color: lightseagreen;
+        background-image: url("https://media.istockphoto.com/id/497159547/photo/home-plate.jpg?s=612x612&w=0&k=20&c=1jXQ-KdU59Ou_G_VFeJCjcvG7gAZx2DODKbIAUYFt8o=");
+    }
+</style>
