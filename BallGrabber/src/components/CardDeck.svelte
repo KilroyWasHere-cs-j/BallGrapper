413ed016183e65e215774825926910659dbaaff6
Gabriel Tower <gmtower1@gmail.com>
2023-11-08 19:55:01 -0500

    Restyled whole page with baseballRed color palete

--- BallGrabber/src/components/CardDeck.svelte
+++ BallGrabber/src/components/CardDeck.svelte
@@ -1,19 +1,19 @@
-<div class="wrapper bg-white rounded-lg">
-  <div class="one border-solid border-2 border-baseballRed rounded-lg">One</div>
-  <div class="two border-solid border-2 border-baseballRed rounded-lg">Two</div>
-  <div class="three border-solid border-2 border-baseballRed rounded-lg">Three</div>
-  <div class="four border-solid border-2 border-baseballRed rounded-lg">Four</div>
-  <div class="five border-solid border-2 border-sky-500 rounded-lg">Five</div>
-  <div class="six border-solid border-2 border-sky-500 rounded-lg">Six</div>
+<div class="wrapper">
+  <div class="one border-solid border-4 shadow-lg border-baseballRed rounded-lg bg-white w-[250px] h-[250px]">Pic One</div>
+  <div class="two border-solid border-4 shadow-lg border-baseballRed rounded-lg bg-white w-[250px] h-[250px]">Pic Two</div>
+  <div class="three border-solid border-4 shadow-lg border-baseballRed rounded-lg bg-white w-[250px] h-[250px]">Pic Three</div>
+  <div class="four border-solid border-4 shadow-lg border-baseballRed rounded-lg bg-white w-[250px] h-[250px]">Pic Four</div>
+  <div class="five border-solid border-4 shadow-lg border-baseballRed rounded-lg bg-white w-[250px] h-[250px]">Pic Five</div>
+  <div class="six border-solid border-4 shadow-lg border-baseballRed rounded-lg bg-white w-[250px] h-[250px]">Pic Six</div>
 </div>
 
 <style>
   .wrapper {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
-  grid-auto-rows: minmax(100px, auto);
-  margin-top: 100px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+    grid-auto-rows: minmax(50px, auto);
+    margin-top: 100px;
 }
 
 .one {
--- BallGrabber/src/routes/+page.svelte
+++ BallGrabber/src/routes/+page.svelte
@@ -17,28 +17,32 @@
 
 <div class="w-full h-full flex flex-col justify-center items-center">
 	<div class="h-56"/>
-	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed" customSize="text-4xl font-extrabold  md:text-5xl lg:text-9xl">Ball Grabber</Heading>
+	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed" customSize="text-4xl font-extrabold md:text-5xl lg:text-9xl">Ball Grabber</Heading>
 </div>
 
 <VBuff/>
 
 <div class="">
-	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">What is the Ball Grabber</Heading>
-	<P class="mb-3 text-center" weight="light" color="text-gray-500 dark:text-gray-950 border-solid border-2 border-sky-500 rounded-lg">Grabs Baseballs... add more</P>
+	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed m-10" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl shadow-lg shadow-baseballRed">What is the Ball Grabber</Heading>
+	<P class="mb-3 text-center" weight="light" color="text-gray-500 dark:text-baseballRed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut etiam sit amet nisl purus. Nullam ac tortor vitae purus faucibus. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Vitae et leo duis ut diam quam nulla porttitor massa. Suspendisse interdum consectetur libero id. Risus in hendrerit gravida rutrum quisque. Tincidunt arcu non sodales neque sodales. Eu ultrices vitae auctor eu augue ut lectus arcu bibendum. Sodales ut etiam sit amet. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Scelerisque purus semper eget duis at tellus at urna. Aliquam etiam erat velit scelerisque in dictum. Nec ultrices dui sapien eget. In nulla posuere sollicitudin aliquam ultrices sagittis orci.
+
+		Pretium fusce id velit ut. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Quam quisque id diam vel quam elementum pulvinar etiam non. Sagittis aliquam malesuada bibendum arcu vitae elementum. Tortor at risus viverra adipiscing at. Enim praesent elementum facilisis leo vel. Tristique senectus et netus et malesuada. Varius vel pharetra vel turpis nunc eget. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Interdum velit laoreet id donec. Risus sed vulputate odio ut enim blandit volutpat. Ultrices sagittis orci a scelerisque purus semper eget. Quam nulla porttitor massa id.</P>
 </div>
 
 <VBuff/>
 
 <div class="">
-	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">How does it work?</Heading>
-	<P class="mb-3 text-center" weight="light" color="text-gray-500 dark:text-baseballRed">Grabs Baseballs... add more</P>
+	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed m-10" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl shadow-lg shadow-baseballRed">How does it work?</Heading>
+	<P class="mb-3 text-center" weight="light" color="text-gray-500 dark:text-baseballRed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sodales ut etiam sit amet nisl purus. Nullam ac tortor vitae purus faucibus. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Vitae et leo duis ut diam quam nulla porttitor massa. Suspendisse interdum consectetur libero id. Risus in hendrerit gravida rutrum quisque. Tincidunt arcu non sodales neque sodales. Eu ultrices vitae auctor eu augue ut lectus arcu bibendum. Sodales ut etiam sit amet. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Scelerisque purus semper eget duis at tellus at urna. Aliquam etiam erat velit scelerisque in dictum. Nec ultrices dui sapien eget. In nulla posuere sollicitudin aliquam ultrices sagittis orci.
+
+		Pretium fusce id velit ut. Cras adipiscing enim eu turpis egestas pretium aenean pharetra magna. Quam quisque id diam vel quam elementum pulvinar etiam non. Sagittis aliquam malesuada bibendum arcu vitae elementum. Tortor at risus viverra adipiscing at. Enim praesent elementum facilisis leo vel. Tristique senectus et netus et malesuada. Varius vel pharetra vel turpis nunc eget. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget. Interdum velit laoreet id donec. Risus sed vulputate odio ut enim blandit volutpat. Ultrices sagittis orci a scelerisque purus semper eget. Quam nulla porttitor massa id.</P>
 </div>
 <CardDeck/>
 
 <VBuff/>
 
 <div class="">
-	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Where to get one</Heading>
+	<Heading tag="h1" class="mb-4 text-gray-500 dark:text-baseballRed m-10" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">Where to get one</Heading>
 	<P class="mb-3 text-center" weight="light" color="text-gray-500 dark:text-baseballRed">Purchasing info</P>
 	<div class="flex content-center justify-center">
 		<ButtonComponent on:click={openOrderPage} symbol={"⚾"} bgColor={"#E63946"}/>
@@ -48,9 +52,14 @@
 <!-- Always keep this at the bottom of the page -->
 <VBuff/>
 
-<style>
+<div class="bg-baseballRed">
+	<span class="w-full h-full flex justify-center items-center text-5xl font-black">RAD</span>
+</div>
+<p class="flex justify-center">Kilroy Was Here</p>
+
+<!-- <style>
 	:global(body){
         background-color: lightseagreen;
         background-image: url("https://media.istockphoto.com/id/497159547/photo/home-plate.jpg?s=612x612&w=0&k=20&c=1jXQ-KdU59Ou_G_VFeJCjcvG7gAZx2DODKbIAUYFt8o=");
     }
-</style>
+</style> -->
